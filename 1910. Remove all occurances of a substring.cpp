62bc201329e6a77f87a972f9a8a5df2be241a73c
shreyansh784 <shreyurk@gmail.com>
2025-02-11 18:54:46 +0530

    11th feb

--- 1910. Remove all occurances of a substring.cpp
+++ 1910. Remove all occurances of a substring.cpp
@@ -1,9 +1,25 @@
+#include <iostream>
+using namespace std;
+
 class Solution {
-    public:
-        string removeOccurrences(string s, string part) {
-            while(s.find(part)!= string::npos){
-                s.erase(s.find(part),part.length());
-            }
-            return s;
+public:
+    string removeOccurrences(string s, string part) {
+        int m = part.size();
+        
+        while (true) {
+            size_t i = s.find(part);
+            if (i == string::npos) break;  // If 'part' not found, exit loop
+            s.erase(i, m);  // Correct erase: remove 'm' characters from index 'i'
         }
-    };
+        return s;
+    }
+};
+
+// Driver Code
+int main() {
+    Solution sol;
+    string s = "daabcbaabcbc", part = "abc";
+    cout << sol.removeOccurrences(s, part) << endl;  // Output: "dab"
+
+    return 0;
+}
